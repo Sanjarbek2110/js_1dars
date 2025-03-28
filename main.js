08a5913c6ffe2e886d150df513576089933ccf60
Sanjarbek2110 <gulnozaaripova45@gmail.com>
2025-03-28 23:03:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const ism = prompt("Ismingizni kiriting");
+const Familiya = prompt("Familiyangizni kiriting");
+const yosh = +prompt("Yoshingizni kiriting");
+const qiziqish = prompt("Qaysi sohaga qiziqasiz");
+const mazil = prompt("Qayerda turasiz");
+const raqam = +prompt("Yoqtirgan raqamingiz");
+const pets = prompt("Yoqtirgan uy hayvoningiz");
+const ish = prompt("Qayerda ishlaysiz");
+
+
+console.log("Ism:" + ism);
+
+console.log("Familiya:" + Familiya );
+
+console.log("Yosh:" + yosh );
+
+console.log("Qiziqish:" + qiziqish );
+
+console.log("Manzil:" + mazil );
+
+console.log("Yoqtirgan raqamingiz:" + raqam );
+
+console.log("Uy hayvoningiz:" + pets );
+
+console.log("Ish joy:" + ish );
+
+alert(
+    "Ism: " + ism + "\n" +
+    "Familiya: " + Familiya + "\n" +
+    "Yosh: " + yosh + "\n" +
+    "Qiziqish: " + qiziqish + "\n" +
+    "Manzil: " + mazil + "\n" +
+    "Yoqtirgan raqamingiz: " + raqam + "\n" +
+    "Uy hayvoningiz: " + pets + "\n" +
+    "Ish joy: " + ish
+);
